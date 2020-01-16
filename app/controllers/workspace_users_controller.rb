f6db82f67cc3b8ff15d4d79372d20c3542b652b6
nakashima <nakashima@biocomputing.cc>
2020-01-17 03:57:41 +0900

    ワークスペースを検索して参加すると自動で#generalに参加する

--- app/controllers/workspace_users_controller.rb
+++ app/controllers/workspace_users_controller.rb
@@ -5,9 +5,12 @@ class WorkspaceUsersController < ApplicationController
   end
 
   def create
-    @workspace_user = current_user.workspace_users.build(workspace_user_params)
-    if @workspace_user.save
-      redirect_to workspaces_path, notice: 'ワークスペースに参加しました。'
+    workspace_user = current_user.workspace_users.build(workspace_user_params)
+    if workspace_user.save
+      workspace = Workspace.find(params[:workspace_user][:workspace_id])
+      room = workspace.rooms.find_by(name: "general")
+      RoomUser.create(user_id: current_user.id, room_id: room.id)
+      redirect_to workspaces_path
     end
   end
 
